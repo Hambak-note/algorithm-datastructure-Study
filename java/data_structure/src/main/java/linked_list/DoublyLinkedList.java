45234711d52d36a8540315b277fc00e0511346c5
Hambak-note <dudghks5722@gmail.com>
2022-11-30 09:07:15 +0900

    feat: 이중 연결리스트 Node 클래스 구현

---
+++ java/data_structure/src/main/java/linked_list/DoublyLinkedList.java
@@ -0,0 +1,55 @@
+package linked_list;
+
+public class DoublyLinkedList<E> implements LinkedList<E>{
+    @Override
+    public void push(E element) {
+
+    }
+
+    @Override
+    public E pop() {
+        return null;
+    }
+
+    @Override
+    public void unShift(E element) {
+
+    }
+
+    @Override
+    public E shift() {
+        return null;
+    }
+
+    @Override
+    public void insert(int index, E element) {
+
+    }
+
+    @Override
+    public E remove(int index) {
+        return null;
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public E findItemByIndex(int index) {
+        return null;
+    }
+
+    private static class Node<E>{
+        E item;
+        Node<E> next;
+        Node<E> prev;
+
+        public Node(E item, Node<E> next, Node<E> prev) {
+            this.item = item;
+            this.next = next;
+            this.prev = prev;
+        }
+    }
+}
