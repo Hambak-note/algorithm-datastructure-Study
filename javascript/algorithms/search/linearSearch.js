813768e869ce7279eca957ff1a32fbc5948d6bca
ukkodeveloper <ukkodeveloper@gmail.com>
2022-12-28 10:01:47 +0900

    feat: linear search javascript

---
+++ javascript/algorithms/search/linearSearch.js
@@ -0,0 +1,10 @@
+const linearSearch = (list, toFind) => {
+  const indexList = list.reduce((acc, elem, index) => {
+    if (elem === toFind) {
+      acc.push(index);
+    }
+    return acc;
+  }, []);
+
+  return indexList.shift() || -1;
+};
